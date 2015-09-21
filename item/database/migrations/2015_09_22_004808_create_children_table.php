cd35a59843719feb8c230a519a56304a62639eec
phper <inaka-phper@users.noreply.github.com>
2015-09-22 00:59:03 +0900

    [add]add `children` DB table.
    users to `hasmany` children table.

---
+++ item/database/migrations/2015_09_22_004808_create_children_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChildrenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('children', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('user_id');
+            $table->date('birth')->nullable()->defulat(null);
+            $table->enum('gender', ['male','female'])->nullable()->default(null);
+            $table->date('plan')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('children');
+    }
+}
