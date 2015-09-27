0f272d6c6a390eb1b40f8cae3fc887fd5cfbbb19
phper <inaka-phper@users.noreply.github.com>
2015-09-27 13:22:12 +0900

    [modify]add email and timestamps to hidden columns.

--- item/app/User.php
+++ item/app/User.php
@@ -35,7 +35,7 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-    protected $hidden = ['password', 'remember_token'];
+    protected $hidden = ['email', 'password', 'remember_token', 'created_at', 'updated_at'];
 
     /**
      * encoding to bcrypt from plain password.
