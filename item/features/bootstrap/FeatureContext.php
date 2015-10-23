55b1c6db3f55ec3f2364061b7df6d76bc9d3b0fc
phper <inaka-phper@users.noreply.github.com>
2015-10-23 15:31:48 +0900

    [add]settings for behat-laravel-extension.

--- item/features/bootstrap/FeatureContext.php
+++ item/features/bootstrap/FeatureContext.php
@@ -2,13 +2,13 @@
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+use PHPUnit_Framework_Assert as PHPUnit;
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context, SnippetAcceptingContext
+class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
 {
     /**
      * Initializes context.
@@ -19,5 +19,16 @@ class FeatureContext implements Context, SnippetAcceptingContext
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Then I should be able to do something with Laravel
+     */
+    public function iShouldBeAbleToDoSomethingWithLaravel()
+    {
+        $environmentFileName = app()->environmentFile();
+        $environmentName = env('APP_ENV');
+        PHPUnit::assertEquals('.env.behat', $environmentFileName);
+        PHPUnit::assertEquals('acceptance', $environmentName);
     }
 }
